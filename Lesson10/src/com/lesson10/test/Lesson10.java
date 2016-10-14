eb19b819e7df1c923e7c00209daf3c3eb44653f0
anhnt-ptit <anhnt.ptit@gmail.com>
2016-10-14 18:21:24 +0700

    this is test comment
    this is test for comment

--- Lesson10/src/com/lesson10/test/Lesson10.java
+++ Lesson10/src/com/lesson10/test/Lesson10.java
@@ -7,6 +7,7 @@ public class Lesson10 {
 		System.out.println("Hello Lesson 10");
 		System.out.println("Hello Lesson 10 33");
 		System.out.println("This is test");
+		System.out.println("This is test");
 	}
 
 }
