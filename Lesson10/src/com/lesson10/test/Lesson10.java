ab6f12c375f26a43c71d1c8fa3859a141d671655
anh ngo <anhnt.ptit@gmail.com>
2016-10-15 10:49:31 +0700

    Init import

---
+++ Lesson10/src/com/lesson10/test/Lesson10.java
@@ -0,0 +1,10 @@
+package com.lesson10.test;
+
+public class Lesson10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Lesson 10");
+	}
+
+}
